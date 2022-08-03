c7dea18c1e6f333ead1732d199cc8daa69932a8d
smoc180320 <smoc123@gmail.com>
2022-08-03 13:40:47 +0700

    input date to export screen

--- 03-08-2022/index.js
+++ 03-08-2022/index.js
@@ -1,3 +1,10 @@
 let yourName = prompt ("Let's input your name");
 // document.write("Hello" + yourName)
-alert("Hello");
+alert("Hello", yourName);
+
+// Bài nhập xuất ngày tháng
+
+d = prompt("Ngày");
+m = prompt("Tháng");
+y = prompt ("Năm");
+document.write(d + "/" + m + "/" + y);
