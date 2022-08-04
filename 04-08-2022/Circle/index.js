1a2426041c7a9e62c3af49ccc7b840b51a839c0f
smoc180320 <smoc123@gmail.com>
2022-08-04 21:15:22 +0700

    Circle

---
+++ 04-08-2022/Circle/index.js
@@ -0,0 +1,7 @@
+const Pi = 3.14;
+r = prompt ("Nhập Bán kính");
+c = 2 * r * Pi;
+s = r**2 * Pi;
+document.write("Chu vi của hình tròn là: C = " + c);
+document.write('<br/>');
+document.write("Diện tích  của hình tròn là: S = " + s);
