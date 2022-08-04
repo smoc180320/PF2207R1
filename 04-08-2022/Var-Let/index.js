e7418d21ff1bb266b538ac565d12cb1ecfc87b90
smoc180320 <smoc123@gmail.com>
2022-08-04 09:55:02 +0700

    Bài khai báo biến và sử dụng biến

---
+++ 04-08-2022/Var-Let/index.js
@@ -0,0 +1,31 @@
+// Practice 1
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội';
+
+document.write('i = ' + i);
+document.write('<br/>');
+document.write('f = ' + f);
+document.write('<br/>');
+document.write('b = ' + b);
+document.write('<br/>');
+document.write('s = ' + s);
+
+// Practice 2
+
+document.write('<br/>');
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('Area = ' + area);
+document.write('<br/>');
+
+// Practice 3
+a = prompt("a");
+b = prompt("b");
+c = a%b;
+kt = "đúng"
+if (c > 0)
+    kt = "sai";
+alert("a là bội số của b: " + kt);
