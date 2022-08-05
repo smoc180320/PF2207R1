0fb62ac354a5646824a22e2dec79b113e2cafa24
smoc180320 <smoc123@gmail.com>
2022-08-05 20:25:32 +0700

    Content mail

---
+++ 04-08-2022/Content Mail/index.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML = prompt("Nhập tên người nhận: ");
+document.getElementById("address").innerHTML = prompt("Nhập địa chỉ: ");
+document.getElementById("date").innerHTML = prompt("Nhập ngày: ");
+document.getElementById("month").innerHTML = prompt("Nhập tháng: ");
+document.getElementById("year").innerHTML = prompt("Nhập năm: ")
