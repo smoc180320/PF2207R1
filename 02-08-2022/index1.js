6ffe1d7d336e15d3d1616d511e84113fdf168973
smoc180320 <smoc123@gmail.com>
2022-08-11 21:28:22 +0700

    update 11-08-22

--- 02-08-2022/index1.js
+++ 02-08-2022/index1.js
@@ -1,5 +1,5 @@
-var intro = "Hello World";
-document.getElementById("intro").innerHTML = intro;
+// var intro = "Hello World";
+// document.getElementById("intro").innerHTML = intro;
 
 function showMessage(){
     alert('Xin chào Cổ Lễ!');
